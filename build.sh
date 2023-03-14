8e2e2cef074012b3633427f0bc46ed3da2b1558c
ValdikSS <iam@valdikss.org.ru>
2023-03-14 21:40:45 +0300

    Build script small fixes

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python -m pip install . --target torparse
 find torparse -path '*/__pycache__*' -delete
-python -m zipapp -m "tor_relay_scanner.scanner:main" -c torparse
+cp torparse/tor_relay_scanner/__main__.py torparse/
+python -m zipapp -c -p '/usr/bin/env python3' torparse
